644c10eddf55c972751b44c6c366c6ce05e56fa7
Daniel G <48115298+dan8kfm@users.noreply.github.com>
2019-03-01 14:48:42 -0800

    conflict

--- DanUpload/src/Hello.java
+++ DanUpload/src/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
         System.out.println("WOOF");
         System.out.println("Chicken: bark bark");
         System.out.println("hello my dudes");
-        System.out.println("LE WOOF");
+        System.out.println("LE WOO");
     }
 }
