6c26088dd5493372718571f4c4b510edb21c11f8
Daniel G <48115298+dan8kfm@users.noreply.github.com>
2019-03-01 12:08:34 -0800

    dan added line to file

--- src/testClass.java
+++ src/testClass.java
@@ -1,12 +1,15 @@
 public class testClass {
 
-    testClass aClass;
-     public testClass()
+    private testClass aClass;
+    String string;
+     public testClass(testClass aClass, String string)
      {
-         this.aClass = new testClass();
+         this.aClass = new testClass(aClass,string);
+         this.string = string;
      }
      public static void main(String[] args)
      {
-         aClass.aClass.aClass;
+         java.lang.Class aClass = short.class;
+         main(aClassargs.length));
      }
 }
