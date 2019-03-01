f2e201704d8e02434420760b272a4f00acda0b29
Daniel G <48115298+dan8kfm@users.noreply.github.com>
2019-03-01 11:52:23 -0800

    dan added line to file

--- src/testClass.java
+++ src/testClass.java
@@ -1,2 +1,12 @@
 public class testClass {
+
+    testClass aClass;
+     public testClass()
+     {
+         this.aClass = new testClass();
+     }
+     public static void main(String[] args)
+     {
+         aClass.aClass.aClass;
+     }
 }
